cd7b9f9189ccb9a3d09f3abfde6aee47039088bd
AKHIL S S <93428847+Akhil234@users.noreply.github.com>
2022-05-30 11:40:48 -0600

    Add files via upload

---
+++ java/bank/bank.java
@@ -0,0 +1,14 @@
+package bank;
+
+ class bank {
+    String name;
+    String email;
+    
+}
+public class bank2
+{
+    public static void main(String args[])
+    bank b1=new bank();
+    b1.name="AFSC";
+    b1.email="gdjdj";
+}
